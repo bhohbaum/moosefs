a604d1196ac4d4bbc08698424e5ec4258775da6f
Botho Hohbaum <Botho.Hohbaum@adrodev.de>
2014-07-08 15:26:38 +0200

    update

--- configure-build-install.sh
+++ configure-build-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var/lib --with-default-user=mfs --with-default-group=mfs
+./configure --prefix=/usr --sysconfdir=/etc/mfs --localstatedir=/var/lib --with-default-user=mfs --with-default-group=mfs
 
 make && make install
 
